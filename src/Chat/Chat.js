a83f351d9558f04da7f5d9229a26f0f9ab54fa18
qianwu2 <qianwu2@ap.xom.com>
2020-07-15 20:54:27 +0800

    chat [qian] make data transfer between in chat and chatInput

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Chat.scss';
 import ChatHeader from './ChatHeader/ChatHeader';
 import ChatBox from './ChatBox/ChatBox';
@@ -18,23 +18,28 @@ class Chat extends Component {
   componentDidMount() {
     const defaultMessage = answersData.find((answer) => answer.tags.includes('DEFAULT'));
     const messages = this.state.messages.concat(defaultMessage);
-
+    console.log("message length: " + this.state.messages.length)
     setTimeout(() => {
       this.setState({
         shop: shopData,
-        messages,
+        messages: this.state.messages.concat(defaultMessage),
       });
     }, 1000);
   }
 
+  setMessage = name => {
+    this.state.messages.push(name)
+    this.setState({shop : this.state.shop, messages: this.state.messages})
+  };
+
   render() {
-    const { shop, messages } = this.state;
+    const {shop, messages} = this.state;
     return (
-      <main className="Chat">
-        <ChatHeader shop={shop} />
-        <ChatBox messages={messages} />
-        <ChatInput />
-      </main>
+        <main className="Chat">
+          <ChatHeader shop={shop}/>
+          <ChatBox messages={this.state.messages}/>
+          <ChatInput onNameChanged={this.setMessage}/>
+        </main>
     );
   }
 }
--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './ChatInput.scss';
+import {ROLE} from "../../constants";
 
 class ChatInput extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: '',
+    };
+  }
+
+  handleInput = event => {
+    this.state.value = event.target.value;
+  };
+
+  handleSubmit = () => {
+    const message = {
+      role: ROLE.CUSTOMER,
+      text: this.state.value
+    };
+    this.props.onNameChanged(message)
+  };
+
   render() {
+
     return (
-      <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
-      </footer>
+        <footer className="ChatInput">
+          <input type="text" onChange={this.handleInput}/>
+          <button type="button" onClick={this.handleSubmit}>Send</button>
+        </footer>
     );
   }
 }
